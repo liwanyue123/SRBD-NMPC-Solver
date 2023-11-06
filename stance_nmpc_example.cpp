05879ebf774acd85312c9147a52a97d3acd38b02
liwanyue123 <995749329@qq.com>
2023-11-06 21:47:25 +0800

    temp

--- stance_nmpc_example.cpp
+++ stance_nmpc_example.cpp
@@ -1,48 +1,8 @@
-// #define EIGEN_USE_BLAS
-// #include <ros/ros.h>
+#include "stance_nmpc_example.h"
 
-// #include <visualization_msgs/Marker.h>
-// #include <visualization_msgs/MarkerArray.h>
-
-#include <eigen3/Eigen/Eigen>
-#include <eigen3/Eigen/Core>
-#include <eigen3/unsupported/Eigen/MatrixFunctions>
-#include <vector>
-#include <string>
-#include <iostream>
-#include "timer/timer.h"
-// #include "hpllt.h"
-#include "hpipm-cpp/include/hpipm-cpp/hpipm-cpp.hpp"
-#include "dynamic/flow_tool.h"
-#include "dynamic/flow_dynamic.h"
-#include "matplotlibcpp.h"
-#include <yaml-cpp/yaml.h>
-// #include <cblas.h>
-
-// void print(const Eigen::MatrixXf& H, const std::string& s = "") {
-//   std::cout << s << std::endl << H << std::endl;
-// }
-
-int main()
+bool readYaml()
 {
-
-  timer timer_test;
-  timer_test.start();
-
-  Eigen::Matrix<double, 12, 1> Q_read;
-  Eigen::Matrix<double, 12, 1> QN_read;
-  Eigen::Matrix<double, 3, 1> L_read;
-  double Qu_read = 0.0;
-  double mu_read = 0.0;
-  double theta_read = 0.0;
-  double alpha_ocp_read = 1.0;
-  int loop_read = 0;
-  bool show_flag_read = false;
-
-  int N_rep_read = 0;
-  int N_step_read = 0;
-  double T_step_read = 0.01;
-
+  // 读取参数
   YAML::Node config;
   try
   {
@@ -84,8 +44,8 @@ int main()
     alpha_ocp_read = config["alpha_ocp"].as<double>();
     loop_read = config["sqp_loop_time"].as<int>();
 
-    N_rep_read = config["N_rep"].as<int>();
-    N_step_read = config["N_step"].as<int>();
+    Nrep = config["N_rep"].as<int>();
+    N = config["N_step"].as<int>();
     T_step_read = config["T_step"].as<double>();
 
     show_flag_read = config["show_flag"].as<bool>();
@@ -93,21 +53,141 @@ int main()
   catch (YAML::BadFile &e)
   {
     std::cout << "read error!" << std::endl;
-    return -1;
+    return false;
   }
+  return true;
+}
 
-  int Nrep = N_rep_read;     // 测试100次
-  const int N = N_step_read; // 预测步
+void initialize()
+{
+  readYaml();
 
-  FlowDynamic flow_dynamic_;
+  // 初始化参数
+  Q.setZero();
+  S.setZero();
+  QN.setZero();
+  Q = Q_read.asDiagonal();
+  R = Qu_read * Eigen::Matrix<double, 12, 12>::Identity(); // 力权重
+  QN = static_cast<double>(N) * QN_read.asDiagonal();      // 设定状态权重
 
-  Eigen::MatrixXd x_nmpc;
-  Eigen::MatrixXd u_nmpc;
+  // 初始化其他变量
+  flow_dynamic_.SetM(15.0);
+  flow_dynamic_.SetT(T_step_read);
+  flow_dynamic_.SetL(L_read.asDiagonal());
+  flow_dynamic_.SetFoot(Eigen::Vector3d(0.0, -0.1, 0.0), Eigen::Vector3d(0.0, 0.1, 0.0), Eigen::Vector3d(1, 1, 1).asDiagonal(), Eigen::Vector3d(1, 1, 1).asDiagonal());
   x_nmpc.resize(12, N + 1);
   u_nmpc.resize(12, N);
-  x_nmpc.setZero();
-  u_nmpc.setZero();
 
+  x_nmpc.setZero();
+  u_nmpc.setOnes();
+  u_nmpc *= 100.0;
+
+  x0.resize(12, 1);
+  x_ref.resize(12, N + 1);
+
+  // hpipm求解参数
+  solver_settings.mode = hpipm::HpipmMode::Speed;
+  solver_settings.iter_max = 30;
+  solver_settings.alpha_min = 1e-8;
+  solver_settings.mu0 = 1e2;
+  solver_settings.tol_stat = 1e-04;
+  solver_settings.tol_eq = 1e-04;
+  solver_settings.tol_ineq = 1e-04;
+  solver_settings.tol_comp = 1e-04;
+  solver_settings.reg_prim = 1e-12;
+  solver_settings.warm_start = 0;
+  solver_settings.pred_corr = 1;
+  solver_settings.ric_alg = 0;
+  solver_settings.split_step = 1;
+}
+
+void drawPlot()
+{
+
+  double box_x = 0.10;
+  double box_y = 0.25;
+  double box_z = 0.50;
+
+  Eigen::Matrix<double, 3, 10> box_list_raw;
+  Eigen::Matrix<double, 3, 6> box_list_;
+  box_list_raw << Eigen::Matrix<double, 3, 1>(0.5 * box_x, 0.5 * box_y, 0.5 * box_z),
+      Eigen::Matrix<double, 3, 1>(0.5 * box_x, 0.5 * box_y, -0.5 * box_z),
+      Eigen::Matrix<double, 3, 1>(0.5 * box_x, -0.5 * box_y, -0.5 * box_z),
+      Eigen::Matrix<double, 3, 1>(0.5 * box_x, -0.5 * box_y, 0.5 * box_z),
+      Eigen::Matrix<double, 3, 1>(0.5 * box_x, 0.5 * box_y, 0.5 * box_z),
+      Eigen::Matrix<double, 3, 1>(-0.5 * box_x, 0.5 * box_y, 0.5 * box_z),
+      Eigen::Matrix<double, 3, 1>(-0.5 * box_x, 0.5 * box_y, -0.5 * box_z),
+      Eigen::Matrix<double, 3, 1>(-0.5 * box_x, -0.5 * box_y, -0.5 * box_z),
+      Eigen::Matrix<double, 3, 1>(-0.5 * box_x, -0.5 * box_y, 0.5 * box_z),
+      Eigen::Matrix<double, 3, 1>(-0.5 * box_x, 0.5 * box_y, 0.5 * box_z);
+
+  if (show_flag_read)
+  {
+    for (int show_loop = 0; show_loop < N + 1; ++show_loop)
+    {
+      Eigen::Matrix<double, 3, 1> r_k = x_nmpc.block<3, 1>(0, show_loop);
+      Eigen::Matrix<double, 3, 1> p_k = x_nmpc.block<3, 1>(6, show_loop);
+      Eigen::Matrix<double, 3, 10> box_list = expm(r_k) * box_list_raw;
+      box_list.colwise() += p_k;
+
+      std::vector<double> x_box;
+      std::vector<double> y_box;
+      std::vector<double> z_box;
+
+      x_box.resize(10);
+
+      y_box.resize(10);
+
+      z_box.resize(10);
+      for (int k_box = 0; k_box < 10; ++k_box)
+      {
+        x_box[k_box] = box_list(0, k_box);
+        y_box[k_box] = box_list(1, k_box);
+        z_box[k_box] = box_list(2, k_box);
+      }
+      matplotlibcpp::figure(1);
+      std::map<std::string, std::string> map_;
+      matplotlibcpp::plot3(x_box, y_box, z_box, map_, 1);
+
+      // d R*I*Rt*w = tau
+      //  R*I*Rt*dw + [w]*R*I*Rt*w*dt = Tau*dt
+
+      std::vector<double> x_sky = {0};
+      std::vector<double> y_sky = {0};
+      std::vector<double> z_sky = {1.0};
+
+      std::vector<double> x_ground = {0};
+      std::vector<double> y_ground = {0};
+      std::vector<double> z_ground = {-0.05};
+
+      matplotlibcpp::xlim(-0.5, 0.5);
+      matplotlibcpp::ylim(-0.5, 0.5);
+
+      // matplotlibcpp::text(0,0,2.0,"sky");
+
+      matplotlibcpp::plot3(x_sky, y_sky, z_sky, map_, 1);
+      matplotlibcpp::plot3(x_ground, y_ground, z_ground, map_, 1);
+      matplotlibcpp::set_zlabel("sky");
+      // matplotlibcpp::
+      // matplotlibcpp::zlim(-1, 1);
+      // matplotlibcpp::plot3({);
+      // matplotlibcpp::show();
+
+      matplotlibcpp::pause(0.1);
+      matplotlibcpp::clf();
+    }
+  }
+  // std::cout << QN_read << std::endl;
+}
+
+int main()
+{
+
+  timer timer_test;
+  timer_test.start();
+  initialize();
+
+  // 单纯测平均速
   for (int nrep = 0; nrep < Nrep; ++nrep)
   {
     flow_dynamic_.SetM(15.0);                                                                                                                                             // 设置质量
@@ -115,98 +195,42 @@ int main()
     flow_dynamic_.SetL(L_read.asDiagonal());                                                                                                                              // 惯量的逆
     flow_dynamic_.SetFoot(Eigen::Vector3d(0.0, -0.1, 0.0), Eigen::Vector3d(0.0, 0.1, 0.0), Eigen::Vector3d(1, 1, 1).asDiagonal(), Eigen::Vector3d(1, 1, 1).asDiagonal()); // 足端位置
 
-    // double dt = 0.02;
-    // double g = 9.8;
-    // double z = 0.6;
-    // double l_zmp = 0.05;
-
-    Eigen::VectorXd x0;
-    x0.resize(12, 1);
     x0 << 0, 0, 0 /*初始轴角*/, 0.0, 0, 0.0 /*初始角速度*/, -0.06, 0, 1.0 /*初始位置*/, 0, 0, 0 /*初始速度*/;
-
-    // Eigen::MatrixXd x_nmpc;
-    // Eigen::MatrixXd u_nmpc;
-    // x_nmpc.resize(12, N+1);
-    // u_nmpc.resize(12, N);
-    x_nmpc.setZero();
-    u_nmpc.setZero();
-    u_nmpc.setOnes();
-    u_nmpc *= 100.0;
-    // Eigen::Matrix<double, 12, 12> S_u;
-    // S_u.setZero();
-    // S_u.diagonal() << 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0;
-    // u_nmpc = S_u*u_nmpc;
-
-    Eigen::VectorXd x_mpc;
-    Eigen::VectorXd x_mpc_next;
-    Eigen::VectorXd u_mpc;
-    x_mpc.resize(12);
-    x_mpc_next.resize(12);
-    u_mpc.resize(12);
-
-    Eigen::Matrix<double, 12, 1> x_ref_k;
-    x_ref_k.setZero();
     x_ref_k << 0.0, 0.0, 0.0 /*目标轴角*/, 0, 0, 0.0 /*目标角速度*/, 0.0, 0, 1.0 /*目标位置*/, 0, 0, 0 /*目标速度*/;
-    Eigen::MatrixXd x_ref;
-    x_ref.resize(12, N + 1);
-
     for (int k_ref = 0; k_ref < N + 1; ++k_ref)
     {
       x_ref.block<12, 1>(0, k_ref) = x_ref_k;
     }
 
-    Eigen::Matrix<double, 12, 12> Q;
-    Q.setZero();
-    // Q.diagonal() << 10,10,10,0.001,0.001,0.001,500,500,500,1,1,1; // 设定状态权重
-    Q = Q_read.asDiagonal();
-    Eigen::Matrix<double, 12, 12> S;
-    S.setZero();
-    // Eigen::Matrix<double, 12, 12> R = 1e-4*Eigen::Matrix<double, 12, 12>::Identity(); // 力权重
-    Eigen::Matrix<double, 12, 12> R = Qu_read * Eigen::Matrix<double, 12, 12>::Identity(); // 力权重
-
-    Eigen::Matrix<double, 12, 12> QN;
-    QN.setZero();
-    // QN.diagonal() << 10*N,10*N,10*N,0.1*N,0.1*N,0.1*N,1000*N,1000*N,1000*N,10*N,10*N,10*N; // 设定状态权重
-    // QN = 1000.0*Q;
-    QN = static_cast<double>(N) * QN_read.asDiagonal();
-
-    Eigen::MatrixXd A_dynamic, B_dynamic, b_dynamic, A_constrain, f_constrain;
-    Eigen::MatrixXd f_constrain_all, f_all;
-
-    f_constrain_all.resize(24, N);
-    f_all.resize(12, N);
-
-    double mu_barrier = mu_read;                              // 障碍方程系数
-    double theta_barrier = theta_read;                        // 障碍方程系数
-    Eigen::VectorXd b_constrain, db_constrain, ddb_constrain; // 障碍方程
-    b_constrain.resize(24);
-    db_constrain.resize(24);
-    ddb_constrain.resize(24);
-
-    // hpipm求解参数
-    hpipm::OcpQpIpmSolverSettings solver_settings;
-    solver_settings.mode = hpipm::HpipmMode::Speed;
-    solver_settings.iter_max = 30;
-    solver_settings.alpha_min = 1e-8;
-    solver_settings.mu0 = 1e2;
-    solver_settings.tol_stat = 1e-04;
-    solver_settings.tol_eq = 1e-04;
-    solver_settings.tol_ineq = 1e-04;
-    solver_settings.tol_comp = 1e-04;
-    solver_settings.reg_prim = 1e-12;
-    solver_settings.warm_start = 0;
-    solver_settings.pred_corr = 1;
-    solver_settings.ric_alg = 0;
-    solver_settings.split_step = 1;
-
     std::vector<hpipm::OcpQp> qp(N + 1);
     std::vector<hpipm::OcpQpSolution> solution(N + 1);
-
+    // sqp为了收敛循环
     for (int sqp_loop = 0; sqp_loop < loop_read; ++sqp_loop)
     {
+      Eigen::VectorXd x_mpc;
+      Eigen::VectorXd x_mpc_next;
+      Eigen::VectorXd u_mpc;
+      x_mpc.resize(12);
+      x_mpc_next.resize(12);
+      u_mpc.resize(12);
+
+      Eigen::MatrixXd A_constrain, f_constrain;
+      Eigen::MatrixXd f_constrain_all, f_all;
+
+      f_constrain_all.resize(24, N);
+      f_all.resize(12, N);
+
+      double mu_barrier = mu_read;                              // 障碍方程系数
+      double theta_barrier = theta_read;                        // 障碍方程系数
+      Eigen::VectorXd b_constrain, db_constrain, ddb_constrain; // 障碍方程
+      b_constrain.resize(24);
+      db_constrain.resize(24);
+      ddb_constrain.resize(24);
 
       for (int i = 0; i < N; ++i)
       {
+        Eigen::MatrixXd A_dynamic, B_dynamic, b_dynamic;
+
         x_mpc = x_nmpc.block<12, 1>(0, i);
         x_mpc_next = x_nmpc.block<12, 1>(0, i + 1);
         u_mpc = u_nmpc.block<12, 1>(0, i);
@@ -409,113 +433,37 @@ int main()
       // std::cout << "f_constrain_all = " << std::endl << f_constrain_all << std::endl;
       // std::cout << "f_all = " << std::endl << f_all << std::endl;
     }
-    std::cout << "迭代次数 : " << nrep << std::endl;
-    std::cout << "求解状态 = " << std::endl
-              << x_nmpc << std::endl; // 求解状态量
-    std::cout << "求解力矩 = " << std::endl
-              << u_nmpc << std::endl; // 求解力
-    std::cout << "最大摩擦锥约束违反量（负数） = " << std::endl
-              << f_constrain_all.minCoeff() << std::endl; //
-    std::cout << "最大动力学方程违反量（正数） = " << std::endl
-              << f_all.cwiseAbs().maxCoeff() << std::endl;
-    std::cout << "动力学方程违反量 = " << std::endl
-              << f_all << std::endl;
+    // std::cout << "迭代次数 : " << nrep << std::endl;
+    // std::cout << "求解状态 = " << std::endl
+    //           << x_nmpc << std::endl; // 求解状态量
+    // std::cout << "求解力矩 = " << std::endl
+    //           << u_nmpc << std::endl; // 求解力
+    // std::cout << "最大摩擦锥约束违反量（负数） = " << std::endl
+    //           << f_constrain_all.minCoeff() << std::endl; //
+    // std::cout << "最大动力学方程违反量（正数） = " << std::endl
+    //           << f_all.cwiseAbs().maxCoeff() << std::endl;
+    // std::cout << "动力学方程违反量 = " << std::endl
+    //           << f_all << std::endl;
   }
-
-  double box_x = 0.10;
-  double box_y = 0.25;
-  double box_z = 0.50;
-
-  Eigen::Matrix<double, 3, 10> box_list_raw;
-  Eigen::Matrix<double, 3, 6> box_list_;
-  box_list_raw << Eigen::Matrix<double, 3, 1>(0.5 * box_x, 0.5 * box_y, 0.5 * box_z),
-      Eigen::Matrix<double, 3, 1>(0.5 * box_x, 0.5 * box_y, -0.5 * box_z),
-      Eigen::Matrix<double, 3, 1>(0.5 * box_x, -0.5 * box_y, -0.5 * box_z),
-      Eigen::Matrix<double, 3, 1>(0.5 * box_x, -0.5 * box_y, 0.5 * box_z),
-      Eigen::Matrix<double, 3, 1>(0.5 * box_x, 0.5 * box_y, 0.5 * box_z),
-      Eigen::Matrix<double, 3, 1>(-0.5 * box_x, 0.5 * box_y, 0.5 * box_z),
-      Eigen::Matrix<double, 3, 1>(-0.5 * box_x, 0.5 * box_y, -0.5 * box_z),
-      Eigen::Matrix<double, 3, 1>(-0.5 * box_x, -0.5 * box_y, -0.5 * box_z),
-      Eigen::Matrix<double, 3, 1>(-0.5 * box_x, -0.5 * box_y, 0.5 * box_z),
-      Eigen::Matrix<double, 3, 1>(-0.5 * box_x, 0.5 * box_y, 0.5 * box_z);
-
   std::cout << "nmpc平均求解时间 = " << timer_test.get() / double(Nrep) << std::endl;
+  // drawPlot();
+  // Eigen::MatrixXd A_test, B_test, b_test, f_test;
+  // Eigen::VectorXd x_test;
+  // Eigen::VectorXd x_test_next;
+  // Eigen::VectorXd u_test;
 
-  if (show_flag_read)
-  {
-    for (int show_loop = 0; show_loop < N + 1; ++show_loop)
-    {
-      Eigen::Matrix<double, 3, 1> r_k = x_nmpc.block<3, 1>(0, show_loop);
-      Eigen::Matrix<double, 3, 1> p_k = x_nmpc.block<3, 1>(6, show_loop);
-      Eigen::Matrix<double, 3, 10> box_list = expm(r_k) * box_list_raw;
-      box_list.colwise() += p_k;
+  // x_test.resize(12);
+  // x_test_next.resize(12);
 
-      std::vector<double> x_box;
-      std::vector<double> y_box;
-      std::vector<double> z_box;
+  // u_test.resize(12);
 
-      x_box.resize(10);
+  // x_test_next.setZero();
 
-      y_box.resize(10);
-
-      z_box.resize(10);
-      for (int k_box = 0; k_box < 10; ++k_box)
-      {
-        x_box[k_box] = box_list(0, k_box);
-        y_box[k_box] = box_list(1, k_box);
-        z_box[k_box] = box_list(2, k_box);
-      }
-      matplotlibcpp::figure(1);
-      std::map<std::string, std::string> map_;
-      matplotlibcpp::plot3(x_box, y_box, z_box, map_, 1);
-
-      // d R*I*Rt*w = tau
-      //  R*I*Rt*dw + [w]*R*I*Rt*w*dt = Tau*dt
-
-      std::vector<double> x_sky = {0};
-      std::vector<double> y_sky = {0};
-      std::vector<double> z_sky = {1.0};
-
-      std::vector<double> x_ground = {0};
-      std::vector<double> y_ground = {0};
-      std::vector<double> z_ground = {-0.05};
-
-      matplotlibcpp::xlim(-0.5, 0.5);
-      matplotlibcpp::ylim(-0.5, 0.5);
-
-      // matplotlibcpp::text(0,0,2.0,"sky");
-
-      matplotlibcpp::plot3(x_sky, y_sky, z_sky, map_, 1);
-      matplotlibcpp::plot3(x_ground, y_ground, z_ground, map_, 1);
-      matplotlibcpp::set_zlabel("sky");
-      // matplotlibcpp::
-      // matplotlibcpp::zlim(-1, 1);
-      // matplotlibcpp::plot3({);
-      // matplotlibcpp::show();
-
-      matplotlibcpp::pause(0.1);
-      matplotlibcpp::clf();
-    }
-  }
-  // std::cout << QN_read << std::endl;
-
-  Eigen::MatrixXd A_test, B_test, b_test, f_test;
-  Eigen::VectorXd x_test;
-  Eigen::VectorXd x_test_next;
-  Eigen::VectorXd u_test;
-
-  x_test.resize(12);
-  x_test_next.resize(12);
-
-  u_test.resize(12);
-
-  x_test_next.setZero();
-
-  double m = 15.0;
-  double g = 9.8;
-  x_test << 0.1, 0.15, 0.25, 0.1, 0.2, 0.3, 0.05, 0.06, 0.5, 0.5, 0.8, 0.1;
-  u_test << 0, 0, 0.5 * m * g, 0.1, 0.2, 0.3, 0, 0, 0.5 * m * g, 0.4, 0.5, 0.6;
-  flow_dynamic_.GetShootingDynamic(x_test, x_test_next, u_test, &A_test, &B_test, &b_test, &f_test); // 获取离散动力学矩阵
+  // double m = 15.0;
+  // double g = 9.8;
+  // x_test << 0.1, 0.15, 0.25, 0.1, 0.2, 0.3, 0.05, 0.06, 0.5, 0.5, 0.8, 0.1;
+  // u_test << 0, 0, 0.5 * m * g, 0.1, 0.2, 0.3, 0, 0, 0.5 * m * g, 0.4, 0.5, 0.6;
+  // flow_dynamic_.GetShootingDynamic(x_test, x_test_next, u_test, &A_test, &B_test, &b_test, &f_test); // 获取离散动力学矩阵
 
   // std::cout << "A_test = " << std::endl << A_test << std::endl; // 求解状态量
   // std::cout << "B_test = " << std::endl << B_test << std::endl; // 求解状态量
